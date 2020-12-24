02b36c514ee7f39cc7356699ca15c8402a277e04
jai2dev <hijack2715jai29@gmail.com>
2020-12-24 23:45:48 +0530

    deployed version 0.1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'; 
+import ji from './img/ji.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>currently in maintainance</h1>
+     <h2>developed by Jaidev</h2>
+     <img src={ji} alt="bg"/>
+    </div>
+  );
+}
+
+export default App;
