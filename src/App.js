ae80ff37f06296d429c951304ff3e11aef6897d0
jai2dev <hijack2715jai29@gmail.com>
2020-12-26 21:42:55 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
-import React from 'react'; 
-import ji from './img/ji.jpg';
+import React,{Component} from 'react'; 
+import ParticlesBg from 'particles-bg';
+import {useDencrypt} from "use-dencrypt-effect";
+
+ 
+
 import './App.css';
 
-function App() {
+const values=["Jaidev Chittoria","I am a Student"];
+
+
+const App = () =>{
+
+  const { result, dencrypt } = useDencrypt();
+
+  React.useEffect(() => {
+    let i = 0;
+
+    const action = setInterval(() => {
+      dencrypt(values[i]);
+
+      i = i === values.length - 1 ? 0 : i + 1;
+    }, 2000);
+
+    return () => clearInterval(action);
+  }, []);
+
+
   return (
-    <div className="App">
-     <h1>currently in maintainance</h1>
-     <h2>developed by Jaidev</h2>
-     <img src={ji} alt="bg"/>
+    <div>
+
+  <h1 style={{textAlign:'center'}}>currently in maintainance</h1>
+ 
+    <div className="App" id="1">
+     
+     <h2>{result}</h2>
+     
     </div>
+
+    <ParticlesBg type="cobweb" bg={true}/>
+
+    </div>
+
+    
+    
   );
-}
+};
 
 export default App;
+// export default Example;
